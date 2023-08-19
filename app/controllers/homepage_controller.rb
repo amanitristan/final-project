827cc4b0278e2115c62e7cabc758818135589a61
Amani T <136659501+amanitristan@users.noreply.github.com>
2023-08-19 20:14:10 +0000

    Formatted app homepage

---
+++ app/controllers/homepage_controller.rb
@@ -0,0 +1,5 @@
+class HomepageController < ApplicationController
+  def home
+    render{( :template => "homepage/home" )}
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get("/", controller: "homepage", action: "home")
+
+  get("/articles", controller: "articles", action: "index")
+  post("/add_article", controller: "articles", action: "create")
+
+  get("/podcasts", controller: "podcasts", action: "index")
+  post("/add_podcast", controller: "podcasts", action: "create")
+
+  get("/videos", controller: "videos", action: "index")
+  post("/add_video", controller: "videos", action: "create")
+
 end
